3fb445c1e18a8ac25fadb70b2da340f595f462bf
AndersMalmgren <anders.malmgren@agero.se>
2012-01-25 23:51:36 +0000

    Added delta filter

--- FreePIE.Core/ScriptEngine/Globals/ScriptHelpers/FilterHelper.cs
+++ FreePIE.Core/ScriptEngine/Globals/ScriptHelpers/FilterHelper.cs
@@ -9,27 +9,39 @@ namespace FreePIE.Core.ScriptEngine.Globals.ScriptHelpers
     [LuaGlobal(Name = "filters")]
     public class FilterHelper : IScriptHelper
     {
-        private readonly Dictionary<string, double> simpleFilterLastSamples;
+        private readonly Dictionary<string, double> lastSamples;
 
         public FilterHelper()
         {
-            simpleFilterLastSamples = new  Dictionary<string, double>();
+            lastSamples = new  Dictionary<string, double>();
         }
 
         [NeedIndexer]
         public double simple(double x, double smoothing, string indexer)
         {
             if(smoothing < 0 && smoothing > 1)
-                throw new ArgumentException("Smooting must be a value between 0 and 1");
+                throw new ArgumentException("Smoothing must be a value between 0 and 1");
 
             var lastSample = x;
-            if (simpleFilterLastSamples.ContainsKey(indexer))
-                lastSample = simpleFilterLastSamples[indexer];
+            if (lastSamples.ContainsKey(indexer))
+                lastSample = lastSamples[indexer];
 
             lastSample = (lastSample*smoothing) + (x*(1 - smoothing));
-            simpleFilterLastSamples[indexer] = lastSample;
+            lastSamples[indexer] = lastSample;
 
             return lastSample;
+        }
+
+        [NeedIndexer]
+        public double delta(double x, string indexer)
+        {
+            var lastSample = x;
+            if (lastSamples.ContainsKey(indexer))
+                lastSample = lastSamples[indexer];
+
+            lastSamples[indexer] = x;
+
+            return lastSample - x;
         }
     }
 }
