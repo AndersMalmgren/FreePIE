cf56efbbe25baffec81cc2d467f77938e049b638
AndersMalmgren <anders.malmgren@agero.se>
2012-07-13 01:18:35 +0200

    Missed file last commmit

---
+++ FreePIE.Core/Model/Events/CurveChangedNameEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FreePIE.Core.Model.Events
+{
+    public class CurveChangedNameEvent
+    {
+        public Curve Curve { get; private set; }
+
+        public CurveChangedNameEvent(Curve curve)
+        {
+            Curve = curve;
+        }
+    }
+}
