bd2298aa13c997038568a66cf752575659006a68
Max Malmgren <pollynation@hotmail.com>
2012-10-26 17:42:57 +0200

    The watch-window is now updated with a 20 millisecond interval instead

--- FreePIE.GUI/Views/Script/Output/WatchesViewModel.cs
+++ FreePIE.GUI/Views/Script/Output/WatchesViewModel.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Timers;
 using Caliburn.Micro;
 using FreePIE.Core.Model.Events;
+using FreePIE.GUI.CodeCompletion.Data;
 using FreePIE.GUI.Events;
 using IEventAggregator = FreePIE.Core.Common.Events.IEventAggregator;
 
@@ -11,10 +13,38 @@ namespace FreePIE.GUI.Views.Script.Output
 {
     public class WatchesViewModel : PropertyChangedBase, Core.Common.Events.IHandle<WatchEvent>, Core.Common.Events.IHandle<ScriptStateChangedEvent>
     {
+        private Timer updateTimer;
+
         public WatchesViewModel(IEventAggregator eventAggregator)
         {
             Watches = new BindableCollection<WatchViewModel>();
+            buffer = new Dictionary<WatchViewModel, object>();
             eventAggregator.Subscribe(this);
+            buffer = new Dictionary<WatchViewModel, object>();
+            updateTimer = new Timer(20);
+            updateTimer.Elapsed += (x, y) => UpdateWatches();
+            updateTimer.Start();
+        }
+
+        private Dictionary<WatchViewModel, object> buffer;
+
+        private void UpdateWatches()
+        {
+            lock (buffer)
+                foreach (var pair in buffer)
+                    pair.Key.Value = pair.Value;
+        }
+
+        private void SetWatchValue(WatchViewModel watch, object value)
+        {
+            lock (buffer)
+                buffer[watch] = value;
+        }
+
+        private void ClearWatchBuffer()
+        {
+            lock(buffer)
+                buffer.Clear();
         }
 
         public void Handle(WatchEvent message)
@@ -42,15 +72,18 @@ namespace FreePIE.GUI.Views.Script.Output
                     }
                 }
             }
-            watch.Value = message.Value;
+            SetWatchValue(watch, message.Value);
 
             return watch;
         }
 
         public void Handle(ScriptStateChangedEvent message)
         {
-            if(message.Running)
+            if (message.Running)
+            {
+                ClearWatchBuffer();
                 Watches.Clear();
+            }
         }
 
         public BindableCollection<WatchViewModel> Watches { get; private set; }
