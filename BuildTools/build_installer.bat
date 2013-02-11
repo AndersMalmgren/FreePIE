d06c38551df7545c1b661e5e23f78b039346b82c
Anders Malmgren <anders.malmgren@agero.se>
2013-02-11 01:55:46 +0100

    Build scripts for WIX installer done

---
+++ BuildTools/build_installer.bat
@@ -0,0 +1,8 @@
+set PATH=%PATH%;%WINDIR%\Microsoft.Net\Framework64\v4.0.30319.
+
+rem go to current folder
+cd %~dp0
+
+msbuild write_VersionInfo.xml
+msbuild build_output.xml /property:OutputTemp=..\OutputTemp /property:BuildDir=..\Output /property:Lib=..\Lib /property:Help=..\FreePIE.Core.Plugins\Help
+msbuild build_installer.xml /property:InstallerTemp=..\InstallerTemp
