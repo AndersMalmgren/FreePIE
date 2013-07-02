8d43a68f01409d7424fd5984bbb573b53517d83e
Max Malmgren <maxmal@kth.se>
2013-07-02 21:37:42 +0200

    Oculus VR C wrapper.

---
+++ Lib/OculusVR/Code/include/ovr_freepie.h
@@ -0,0 +1,14 @@
+#ifndef OVR_FREEPIE_3DOF_H
+#define OVR_FREEPIE_3DOF_H
+
+typedef struct ovr_freepie_3dof
+{
+  float yaw, pitch, roll;
+} ovr_freepie_3dof;
+
+int ovr_freepie_init();
+int ovr_freepie_read(ovr_freepie_3dof *output);
+int ovr_freepie_destroy();
+int ovr_freepie_reset_orientation();
+
+#endif
---
+++ Lib/OculusVR/Code/src/ovr_freepie.cpp
@@ -0,0 +1,60 @@
+extern "C"
+{
+  #include "../include/ovr_freepie.h"
+}
+#include <OVR.h>
+
+using namespace OVR;
+
+Ptr<DeviceManager> pManager = 0;
+Ptr<HMDDevice>     pHMD = 0;
+Ptr<SensorDevice>  pSensor = 0;
+SensorFusion       FusionResult;
+
+int ovr_freepie_init()
+{
+   OVR::System::Init();
+     
+  pManager = *DeviceManager::Create();
+  pHMD     = *pManager->EnumerateDevices<HMDDevice>().CreateDevice();
+
+  if (!pHMD)
+      return 1;
+
+  pSensor  = *pHMD->GetSensor();
+
+  HMDInfo hmdInfo;
+  pHMD->GetDeviceInfo(&hmdInfo);
+    
+  if (pSensor)
+      FusionResult.AttachToSensor(pSensor);
+
+  return 0;
+}
+
+int ovr_freepie_reset_orientation()
+{
+  FusionResult.Reset();
+  return 0;
+}
+
+int ovr_freepie_read(ovr_freepie_3dof *output)
+{
+  Quatf q = FusionResult.GetOrientation();
+
+  Matrix4f bodyFrameMatrix(q); 
+
+  q.GetEulerAngles<Axis_Y, Axis_X, Axis_Z>(&output->yaw, &output->pitch, &output->roll);
+
+  return 0;
+}
+
+int ovr_freepie_destroy()
+{
+  pSensor.Clear();
+  pHMD.Clear();
+  pManager.Clear();
+
+  OVR::System::Destroy();
+  return 0;
+}
