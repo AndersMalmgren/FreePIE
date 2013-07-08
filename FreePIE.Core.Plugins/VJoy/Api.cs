0c507780bbc694b19d3fff81d9a2ea30350fddd2
AndersMalmgren <anders.malmgren@agero.se>
2013-07-08 16:45:00 +0200

    Added support for VJoy

---
+++ FreePIE.Core.Plugins/VJoy/Api.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace FreePIE.Core.Plugins.VJoy
+{
+    public static class Api
+    {
+        [DllImport("VJoy.dll", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool VJoy_Initialize(StringBuilder name, StringBuilder serial);
+
+        [DllImport("VJoy.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern void VJoy_Shutdown();
+
+        [DllImport("VJoy.dll", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool VJoy_UpdateJoyState(int id, ref VJoyState joyState);
+
+        public static bool Initialize()
+        {
+            return VJoy_Initialize(new StringBuilder(string.Empty), new StringBuilder(string.Empty));
+        }
+
+        public static void Dispose()
+        {
+            VJoy_Shutdown();
+        }
+
+        public static bool Update(int id, VJoyState state)
+        {
+            return VJoy_UpdateJoyState(id, ref state);
+        }
+    }
+}
