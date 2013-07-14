ef8b292bcd8010fc0323887c37b6ccb81f3a1c46
Anders Malmgren <anders.malmgren@agero.se>
2013-07-14 23:39:26 +0200

    Moved continues yaw implementation to a filter helper so that it can be used for any rotational input

---
+++ FreePIE.Core/ScriptEngine/Globals/ScriptHelpers/Strategies/ContinuesRotationStrategy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FreePIE.Core.ScriptEngine.Globals.ScriptHelpers.Strategies
+{
+    internal class ContinuesRotationStrategy
+    {
+        private readonly Units unit;
+        private double previousSample;
+
+        public ContinuesRotationStrategy(Units unit)
+        {
+            this.unit = unit;
+        }
+
+        public void Update(double x)
+        {
+            double delta = x - previousSample;
+
+            double halfCircle = unit == Units.Radians ? Math.PI : 180d;
+
+            if (Math.Abs(delta) > halfCircle)
+            {
+                if (delta > 0)
+                    delta -= (2*halfCircle);
+                else
+                    delta += (2*halfCircle);
+            }
+            Out += delta;
+            previousSample = x;
+        }
+
+        public double Out { get; private set; }
+
+    }
+}
