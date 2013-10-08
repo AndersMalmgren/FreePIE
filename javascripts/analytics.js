754dcdb9d84da88430fada5094369aecd4f4deaa
Anders Malmgren <anders.malmgren@agero.se>
2013-10-08 18:02:45 +0200

    Fixed better outbound link tracking

--- javascripts/analytics.js
+++ javascripts/analytics.js
@@ -2,15 +2,19 @@
     $("a.ga-track").click(function () {
         var anchor = this;
         try {
-            _gaq.push(["_trackEvent", "External links", anchor.hostname]);
+            _gaq.push(["_trackEvent", "External links", anchor.href]);
         } catch (err) {
             console.log(err);
         }
+        
+        if ($(this).attr("target") !== "_blank") {
+            setTimeout(function () {
+                document.location.href = anchor.href;
+            }, 100);
+            return false;
+        }
 
-        setTimeout(function () {
-            document.location.href = anchor.href;
-        }, 100);
-        return false;
+        return true;
     });
 })();
 
