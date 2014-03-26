77db95a84be18924298eafa516769cda218d6749
Max Malmgren <maxmal@kth.se>
2014-03-26 21:07:03 +0100

    Refined log message to support inner exceptions better

--- FreePIE.GUI/Bootstrap/BootStrapper.cs
+++ FreePIE.GUI/Bootstrap/BootStrapper.cs
@@ -29,7 +29,6 @@ namespace FreePIE.GUI.Bootstrap
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             kernel.Get<IPersistanceManager>().Load();
-
             base.OnStartup(sender, e);
         }
 
@@ -45,20 +44,36 @@ namespace FreePIE.GUI.Bootstrap
 
         protected override void OnUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            Log(e.Exception);
+            Log(e.Exception, 0);
             base.OnUnhandledException(sender, e);
         }
 
-        private void Log(Exception e)
+        private string PrependTabsToLinebreaks(string input, int numberOfTabs)
         {
-            if (e == null) return;
+            var tabs = new string('\t', numberOfTabs);
+
+            return tabs + input.Replace(Environment.NewLine, Environment.NewLine + tabs);
+        }
+
+        private void Log(Exception e, int indentation)
+        {
+            if (e == null)
+                return;
 
             var fileSystem = kernel.Get<IFileSystem>();
             var paths = kernel.Get<IPaths>();
-            var log = string.Format("{0} - {1}: {2}{3}{3}", DateTime.Now, e.Message, e.StackTrace, Environment.NewLine);
-            fileSystem.AppendAllText(paths.GetDataPath("FreePIE.log"), log);
 
-            Log(e.InnerException);
+            var path = paths.GetDataPath("FreePIE.log");
+
+            var delimiter = indentation == 0 ? DateTime.Now.ToString() + " - " : string.Empty;
+
+            var log = string.Format("{0}{1}{2} - {3}: {5}{4}{5}", new string('\t', indentation), delimiter, e.GetType().FullName, e.Message, PrependTabsToLinebreaks(e.StackTrace, indentation), Environment.NewLine);
+            fileSystem.AppendAllText(path, log);
+
+            Log(e.InnerException, indentation + 1);
+
+            if (indentation == 0)
+                fileSystem.AppendAllText(path, Environment.NewLine);
         }
 
         private void SetupCustomMessageBindings()
