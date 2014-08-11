847fb0fc6a65e6f1ad726486b1124fca0fed4fe7
David G. Miguel <noxwings@gmail.com>
2014-08-12 01:50:03 +0200

    PSMovePlugin file added

---
+++ FreePIE.Core.Plugins/PSMovePlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FreePIE.Core.Contracts;
+
+namespace FreePIE.Core.Plugins
+{
+    [GlobalType(Type = typeof(PSMoveGlobal), IsIndexed = true)]
+    public class PSMovePlugin : Plugin
+    {
+
+        public override object CreateGlobal()
+        {
+            // TODO
+            return null;
+        }
+
+        public override string FriendlyName
+        {
+            get { return "PSMove"; }
+        }
+
+    }
+
+    [Global(Name = "psmove")]
+    public class PSMoveGlobal : UpdateblePluginGlobal<PSMovePlugin>
+    {
+         public PSMoveGlobal(PSMovePlugin plugin) : base(plugin) { }
+    }
+}
