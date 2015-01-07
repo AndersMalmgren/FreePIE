3cf3974ac301955ed5d665bf7447db68d190050f
AndersMalmgren <anders.malmgren@agero.se>
2015-01-08 00:50:59 +0100

    Added midi support

---
+++ FreePIE.Core.Plugins/Midi/DataGlobal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FreePIE.Core.Plugins.Midi
+{
+    public class DataGlobal
+    {
+        public DataGlobal(int message)
+        {
+           // var integer = BitConverter.ToInt32(new byte[] {0xbC, 0x14, 0x7f, 0x0}, 0);
+
+            var data = BitConverter.GetBytes(message);
+            status = (MidiStatus)(data[0] >> 4);
+            channel = (byte)(0x0F & data[0]);
+            buffer = new byte[2];
+            Array.Copy(data, 1, buffer, 0, 2);
+        }
+
+        public MidiStatus status { get; private set; }
+        public byte channel { get; private set; }
+        public byte[] buffer { get; private set; }
+    }
+}
--- FreePIE.Core.Plugins/MidiPlugin.cs
+++ FreePIE.Core.Plugins/MidiPlugin.cs
@@ -1,64 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using CannedBytes.Midi;
 using FreePIE.Core.Contracts;
 using FreePIE.Core.Plugins.Globals;
+using FreePIE.Core.Plugins.Midi;
 
 namespace FreePIE.Core.Plugins
 {
-    [GlobalType(Type = typeof(MidiGlobal), IsIndexed = true)]
+    [GlobalType(Type = typeof (MidiGlobal), IsIndexed = true)]
     public class MidiPlugin : Plugin
     {
+        private List<MidiGlobalHolder> holders;
+
         public override object CreateGlobal()
         {
+            holders = new List<MidiGlobalHolder>();
             return new GlobalIndexer<MidiGlobal>(Create);
         }
 
         private MidiGlobal Create(int index)
         {
             var holder = new MidiGlobalHolder(index);
+            holders.Add(holder);
             return holder.Global;
+        }
+
+        public override void DoBeforeNextExecute()
+        {
+            holders.ForEach(h => h.Update());
+        }
+
+        public override void Stop()
+        {
+            holders.ForEach(h => h.Dispose());
         }
 
         public override string FriendlyName
         {
-            get { return "Midi"; }
+            get { return "MIDI"; }
         }
     }
 
-    public class MidiGlobalHolder : IUpdatable
+    public class MidiGlobalHolder : IUpdatable, IMidiDataReceiver, IDisposable
     {
-        private readonly Dictionary<int, byte> data;
+        private readonly Queue<int> messages = new Queue<int>();
+        private readonly MidiInPort port;
+
 
         public MidiGlobalHolder(int index)
         {
             Global = new MidiGlobal(this);
-            data = new Dictionary<int, byte>();
+            port = new MidiInPort();
+            port.Successor = this;
+
+            var count = new MidiInPortCapsCollection().Count;
+            if(index >= count) 
+                throw new Exception(string.Format("MIDI device with port index {0} not found", index));
+
+            port.Open(index);
+            port.Start();
+        }
+
+        public void Update()
+        {
+            if (messages.Count != 0)
+            {
+                lock (messages)
+                {
+                    while (messages.Count > 0)
+                    {
+                        lastData = new DataGlobal(messages.Dequeue());
+                        OnUpdate();
+                    }
+                }
+            }
         }
 
         public Action OnUpdate { get; set; }
         public bool GlobalHasUpdateListener { get; set; }
         public MidiGlobal Global { get; private set; }
 
-        public byte GetControl(int index)
+        private DataGlobal lastData;
+
+        public DataGlobal Data
         {
-            if (!data.ContainsKey(index)) return 0;
-            return data[index];
+            get { return lastData; }
         }
+
+        public void ShortData(int data, long timestamp)
+        {
+            lock (messages)
+            {
+                messages.Enqueue(data);
+            }
+        }
+
+        public void Dispose()
+        {
+            port.Stop();
+            port.Close();
+        }
+
+        public void LongData(MidiBufferStream buffer, long timestamp){ }
     }
 
     [Global(Name = "midi")]
     public class MidiGlobal : UpdateblePluginGlobal<MidiGlobalHolder>
     {
-        public MidiGlobal(MidiGlobalHolder plugin) : base(plugin)
+        public MidiGlobal(MidiGlobalHolder plugin)
+            : base(plugin)
         {
-            
+
         }
 
-        public byte getControl(int index)
+        public DataGlobal data
         {
-            return plugin.GetControl(index);
+            get { return plugin.Data; }
         }
     }
 }
+
