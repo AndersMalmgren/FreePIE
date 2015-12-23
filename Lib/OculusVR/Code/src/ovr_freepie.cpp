7096d488c34b070f10dc19f61ab080ba46589795
AndersMalmgren <anders.malmgren@agero.se>
2015-12-23 11:14:35 +0100

    OVR 0.8

--- Lib/OculusVR/Code/src/ovr_freepie.cpp
+++ Lib/OculusVR/Code/src/ovr_freepie.cpp
@@ -7,18 +7,23 @@ extern "C"
 using namespace OVR;
 
 ovrHmd	HMD;
+double	HmdFrameTiming;
 
 int ovr_freepie_init()
 {
-	ovr_Initialize();
-	HMD = ovrHmd_Create(0);
+	ovrResult result = ovr_Initialize(nullptr);
+	if (!OVR_SUCCESS(result)) return 1;
+
+	ovrGraphicsLuid luid;
+	result = ovr_Create(&HMD, &luid);
+
+	if (!OVR_SUCCESS(result)) return 1;
+
 	if (!HMD) {
 		return 1;
 	}
 
-	ovrHmd_SetEnabledCaps(HMD, ovrHmdCap_DynamicPrediction);
-
-	ovrHmd_ConfigureTracking(HMD, ovrTrackingCap_Orientation |
+	ovr_ConfigureTracking(HMD, ovrTrackingCap_Orientation |
 		ovrTrackingCap_MagYawCorrection |
 		ovrTrackingCap_Position, 0);
 
@@ -27,13 +32,14 @@ int ovr_freepie_init()
 
 int ovr_freepie_reset_orientation()
 {
-	ovrHmd_RecenterPose(HMD);
+	ovr_RecenterPose(HMD);
 	return 0;
 }
 
 int ovr_freepie_read(ovr_freepie_6dof *output)
 {
-	ovrTrackingState ts = ovrHmd_GetTrackingState(HMD, ovr_GetTimeInSeconds());
+	HmdFrameTiming = ovr_GetPredictedDisplayTime(HMD, 0);
+	ovrTrackingState ts = ovr_GetTrackingState(HMD, ovr_GetTimeInSeconds(), HmdFrameTiming);
 	Posef pose = ts.HeadPose.ThePose;
 	
 	pose.Rotation.GetEulerAngles<Axis_Y, Axis_X, Axis_Z>(&output->yaw, &output->pitch, &output->roll);
@@ -46,7 +52,7 @@ int ovr_freepie_read(ovr_freepie_6dof *output)
 
 int ovr_freepie_destroy()
 {
-	ovrHmd_Destroy(HMD); 
+	ovr_Destroy(HMD); 
 	ovr_Shutdown();
 
 	return 0;
