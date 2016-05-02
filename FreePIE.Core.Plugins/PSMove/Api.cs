b7951ef894759ff05d473e2ea249889ec43aa95f
Anders Malmgren <anders.malmgren@insclear.se>
2016-05-02 15:06:10 +0200

    Added psmove plugin based on the Move framework

---
+++ FreePIE.Core.Plugins/PSMove/Api.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using FreePIE.Core.Contracts;
+
+namespace FreePIE.Core.Plugins.PSMove
+{
+    [GlobalEnum]
+    public enum MoveButton
+    {
+        T1 = 0x1,
+        T2 = 0x4,
+        TRIANGLE = 0x10,
+        CIRCLE = 0x20,
+        CROSS = 0x40,
+        SQUARE = 0x80,
+        SELECT = 0x100,
+        START = 0x800,
+        STICK = 0x200,
+        UP = 0x1000,
+        RIGHT = 0x2000,
+        DOWN = 0x4000,
+        LEFT = 0x8000,
+        PS = 0x10000,
+        MOVE = 0x80000,
+        T = 0x100000
+    };
+
+    public static class Api
+    {
+        public delegate void MoveUpdateCallback(int id, [MarshalAs(UnmanagedType.Struct)] Vector3 position, [MarshalAs(UnmanagedType.Struct)] Quaternion orientation, int trigger);
+        public delegate void NavUpdateCallback(int id, int trigger1, int trigger2, int stickX, int stickY);
+        public delegate void MoveKeyCallback(int id, int keyCode);
+        private static MoveUpdateCallback updateCallback;
+        private static MoveKeyCallback keyDownCallback;
+        private static MoveKeyCallback keyUpCallback;
+        private static NavUpdateCallback navUpdateCallback;
+        private static MoveKeyCallback navKeyDownCallback;
+        private static MoveKeyCallback navKeyUpCallback;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Vector3
+        {
+            public float x;
+            public float y;
+            public float z;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Quaternion
+        {
+            public float w;
+            public float x;
+            public float y;
+            public float z;
+        }
+
+        [DllImport("MF_CWrapper.dll")]
+        public static extern void init();
+        [DllImport("MF_CWrapper.dll")]
+        public static extern int getMovesCount();
+        [DllImport("MF_CWrapper.dll")]
+        public static extern int getNavsCount();
+        [DllImport("MF_CWrapper.dll")]
+        public static extern void unsubscribeMove();
+        [DllImport("MF_CWrapper.dll")]
+        private static extern void subscribeMove(MoveUpdateCallback updateCallback, MoveKeyCallback keyDownCallback, MoveKeyCallback keyUpCallback, NavUpdateCallback navCallback, MoveKeyCallback navKeyDown, MoveKeyCallback navKeyUp);
+        [DllImport("MF_CWrapper.dll")]
+        [return: MarshalAs(UnmanagedType.Struct)]
+        public static extern Quaternion getOrientation(int id);
+        [DllImport("MF_CWrapper.dll")]
+        [return: MarshalAs(UnmanagedType.Struct)]
+        public static extern Vector3 getPosition(int id);
+        [DllImport("MF_CWrapper.dll")]
+        public static extern bool getButtonState(int id, [MarshalAs(UnmanagedType.I4)] MoveButton keyId);
+        [DllImport("MF_CWrapper.dll")]
+        public static extern int getTriggerValue(int id);
+        [DllImport("MF_CWrapper.dll")]
+        public static extern void setRumble(int id, int value);
+        [DllImport("MF_CWrapper.dll")]
+        public static extern int getNavTrigger1(int id);
+        [DllImport("MF_CWrapper.dll")]
+        public static extern int getNavTrigger2(int id);
+        [DllImport("MF_CWrapper.dll")]
+        public static extern int getNavStickX(int id);
+        [DllImport("MF_CWrapper.dll")]
+        public static extern int getNavStickY(int id);
+
+        public static void SubscribeMoveUpdate(MoveUpdateCallback updateCallback, MoveKeyCallback keyDownCallback, MoveKeyCallback keyUpCallback, NavUpdateCallback navCallback, MoveKeyCallback navKeyDown, MoveKeyCallback navKeyUp)
+        {
+            Api.updateCallback = updateCallback;
+            Api.keyDownCallback = keyDownCallback;
+            Api.keyUpCallback = keyUpCallback;
+            Api.navUpdateCallback = navCallback;
+            Api.navKeyDownCallback = navKeyDown;
+            Api.navKeyUpCallback = navKeyUp;
+
+            subscribeMove(updateCallback, keyDownCallback, keyUpCallback, navCallback, navKeyDown, navKeyUp);
+        }
+    }
+}
---
+++ FreePIE.Core.Plugins/PSMovePlugin.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FreePIE.Core.Contracts;
+using FreePIE.Core.Plugins.Globals;
+using FreePIE.Core.Plugins.PSMove;
+using FreePIE.Core.Plugins.SensorFusion;
+using FreePIE.Core.Plugins.Strategies;
+
+namespace FreePIE.Core.Plugins
+{
+    [GlobalType(Type = typeof(PSMoveGlobal), IsIndexed = true)]
+    public class PSMovePlugin : Plugin
+    {
+        private Dictionary<int, PSMoveGlobalHolder> globalHolders; 
+
+        public override object CreateGlobal()
+        {
+            globalHolders = new Dictionary<int, PSMoveGlobalHolder>();
+            return new GlobalIndexer<PSMoveGlobal>(id =>
+            {
+                globalHolders[id] = new PSMoveGlobalHolder();
+                return globalHolders[id].Global;
+            });
+        }
+
+        public override string FriendlyName { get { return "PS Move"; } }
+
+        public override Action Start()
+        {
+            Api.init();
+            Api.SubscribeMoveUpdate(MoveUpdateCallback, MoveKeyDownCallback, MoveKeyUpCallback, NavUpdateCallback, NavKeyDownCallback, NavKeyUpCallback);
+
+            return null;
+        }
+
+        public override void DoBeforeNextExecute()
+        {
+            foreach (var global in globalHolders.Values)
+                global.Update();
+        }
+
+        public override void Stop()
+        {
+            Api.unsubscribeMove();
+        }
+
+        private void MoveUpdateCallback(int id, Api.Vector3 position, Api.Quaternion orientation, int trigger)
+        {
+            Call(id, g => g.MoveUpdate(position, orientation, trigger));
+
+            //UNCOMMENT if you want to see the position and orientation updates
+            Console.WriteLine("UPDATE moveid: " + id +
+                " position: " + position.x + " " + position.y + " " + position.z +
+                " orientation:" + orientation.w + " " + orientation.x + " " + orientation.y + " " + orientation.z + " trigger: " + trigger);
+        }
+
+        private void MoveKeyUpCallback(int id, int keyCode)
+        {
+            Call(id, g => g.ButtonHandler.KeyUpUpdate(keyCode));
+
+            Console.WriteLine("KEYUP moveid: " + id + " keyname: " + Enum.GetName(typeof(MoveButton), keyCode));
+        }
+
+        private void MoveKeyDownCallback(int id, int keyCode)
+        {
+            Call(id, g => g.ButtonHandler.KeyDownUpdate(keyCode));
+
+            Console.WriteLine("KEYDOWN moveid: " + id + " keyname: " + Enum.GetName(typeof(MoveButton), keyCode));
+        }
+
+        private void NavUpdateCallback(int id, int trigger1, int trigger2, int stickX, int stickY)
+        {
+            Call(id, g => g.NavUpdate(trigger1, trigger2, stickX, stickY));
+
+            //UNCOMMENT if you want to see the trigger and stick updates
+            Console.WriteLine("UPDATE navid: " + id + " trigger1: " + trigger1 + " trigger2: " + trigger2 +
+                " stickX: " + stickX + " stickY:" + stickY);
+        }
+
+        private void NavKeyUpCallback(int id, int keyCode)
+        {
+            Call(id, g => g.NavButtonHandler.KeyUpUpdate(keyCode));
+
+            Console.WriteLine("NAV KEYUP navid: " + id + " keyname: " + Enum.GetName(typeof(MoveButton), keyCode));
+        }
+
+        private void NavKeyDownCallback(int id, int keyCode)
+        {
+            Call(id, g => g.NavButtonHandler.KeyDownUpdate(keyCode));
+
+            Console.WriteLine("NAV KEYDOWN navid: " + id + " keyname: " + Enum.GetName(typeof(MoveButton), keyCode));
+        }
+
+        private void Call(int id, Action<PSMoveGlobalHolder> call)
+        {
+            if (globalHolders.ContainsKey(id))
+                call(globalHolders[id]);
+        }
+    }
+
+    public class MoveButtonHandler
+    {
+        private readonly Dictionary<MoveButton, bool> buttonState = new Dictionary<MoveButton, bool>();
+        private readonly GetPressedStrategy<MoveButton> getPressedStrategy;
+
+        public MoveButtonHandler()
+        {
+            getPressedStrategy = new GetPressedStrategy<MoveButton>(IsButtonDown);
+        }
+
+        public bool IsButtonDown(MoveButton button)
+        {
+            return buttonState.ContainsKey(button) && buttonState[button];
+        }
+
+        public bool IsButtonPressed(MoveButton button)
+        {
+            return getPressedStrategy.IsPressed(button);
+        }
+
+        public void KeyUpUpdate(int keyCode)
+        {
+            buttonState[(MoveButton)keyCode] = false;
+        }
+
+        public void KeyDownUpdate(int keyCode)
+        {
+            buttonState[(MoveButton)keyCode] = true;
+        }
+    }
+
+    public class PSMoveGlobalHolder : IUpdatable
+    {
+        private readonly Quaternion quaternion = new Quaternion();
+        private readonly MoveButtonHandler buttonHandler = new MoveButtonHandler();
+        public MoveButtonHandler ButtonHandler { get { return buttonHandler; } }
+
+        private readonly MoveButtonHandler navButtonHandler = new MoveButtonHandler();
+        public MoveButtonHandler NavButtonHandler { get { return navButtonHandler; } }
+
+        public PSMoveGlobalHolder()
+        {
+            Global = new PSMoveGlobal(this);
+        }
+
+        public void Update()
+        {
+            if (NewData)
+                OnUpdate();
+        }
+
+        public bool NewData { get; set; }
+        public PSMoveGlobal Global { get; private set; }
+        public Action OnUpdate { get; set; }
+        public bool GlobalHasUpdateListener { get; set; }
+
+        public void MoveUpdate(Api.Vector3 position, Api.Quaternion q, int trigger)
+        {
+            Global.x = position.x;
+            Global.y = position.y;
+            Global.z = position.z;
+
+            quaternion.Update(q.w, q.x, q.y, q.z);
+
+            Global.yaw = quaternion.Yaw;
+            Global.pitch = quaternion.Pitch;
+            Global.roll = quaternion.Roll;
+
+            Global.trigger = trigger;
+
+            NewData = true;
+        }
+
+        public void NavUpdate(int trigger1, int trigger2, int stickX, int stickY)
+        {
+            Global.trigger1 = trigger1;
+            Global.trigger2 = trigger2;
+            Global.stickX = stickX;
+            Global.stickY = stickY;
+        }
+    }
+
+    [Global(Name = "psmove")]
+    public class PSMoveGlobal : UpdateblePluginGlobal<PSMoveGlobalHolder>
+    {
+        public PSMoveGlobal(PSMoveGlobalHolder plugin) : base(plugin) { }
+
+        public float x { get; internal set; }
+        public float y { get; internal set; }
+        public float z { get; internal set; }
+
+
+        public double yaw { get; internal set; }
+    
+        public double pitch { get; internal set; }
+        public double roll { get; internal set; }
+
+        public int trigger { get; internal set; }
+        public int trigger1 { get; internal set; }
+        public int trigger2 { get; internal set; }
+        public int stickX { get; internal set; }
+        public int stickY { get; internal set; }
+
+        public bool getDown(MoveButton button)
+        {
+            return plugin.ButtonHandler.IsButtonDown(button);
+        }
+
+        public bool getPressed(MoveButton button)
+        {
+            return plugin.ButtonHandler.IsButtonPressed(button);
+        }
+
+        public bool getNavDown(MoveButton button)
+        {
+            return plugin.ButtonHandler.IsButtonDown(button);
+        }
+
+        public bool getNavPressed(MoveButton button)
+        {
+            return plugin.ButtonHandler.IsButtonPressed(button);
+        }
+    }
+}
