06182a9a72e03bf575a3cc35975939ad1fba25e0
Trevor <jones1876@gmail.com>
2016-12-31 23:44:19 -0500

    fixed sample script

--- FreePIE.Core.Plugins/OculusVR/sample.py
+++ FreePIE.Core.Plugins/OculusVR/sample.py
@@ -14,7 +14,7 @@ diagnostics.watch(oculusVR.headPose.yawDegrees)
 diagnostics.watch(oculusVR.headPose.pitchDegrees)
 diagnostics.watch(oculusVR.headPose.rollDegrees)
 
-if oculusVR.headIsTracking:
+if oculusVR.isHeadTracking:
 	diagnostics.watch(oculusVR.headPose.x)
 	diagnostics.watch(oculusVR.headPose.y)
 	diagnostics.watch(oculusVR.headPose.z)
@@ -23,7 +23,7 @@ diagnostics.watch(oculusVR.leftTouchPose.yawDegrees)
 diagnostics.watch(oculusVR.leftTouchPose.pitchDegrees)
 diagnostics.watch(oculusVR.leftTouchPose.rollDegrees)
 
-if oculusVR.leftTouchIsTracking:
+if oculusVR.isLeftTouchTracking:
 	diagnostics.watch(oculusVR.leftTouchPose.x)
 	diagnostics.watch(oculusVR.leftTouchPose.y)
 	diagnostics.watch(oculusVR.leftTouchPose.z)
@@ -32,7 +32,7 @@ diagnostics.watch(oculusVR.rightTouchPose.yawDegrees)
 diagnostics.watch(oculusVR.rightTouchPose.pitchDegrees)
 diagnostics.watch(oculusVR.rightTouchPose.rollDegrees)
 
-if oculusVR.rightTouchIsTracking:
+if oculusVR.isRightTouchTracking:
 	diagnostics.watch(oculusVR.rightTouchPose.x)
 	diagnostics.watch(oculusVR.rightTouchPose.y)
 	diagnostics.watch(oculusVR.rightTouchPose.z)
